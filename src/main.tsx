093b24b7b4f7ae9d668f78f6ef738ac449add104
OHRRRRR <3819713@naver.com>
2024-10-21 21:05:15 +0900

    feat - Market 페이지 구현중

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RecoilRoot } from "recoil";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+import { router } from "./router/router";
 import theme from "./style/theme";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import GlobalStyle from "./style/GlobalStyle";
@@ -11,13 +11,13 @@ import GlobalStyle from "./style/GlobalStyle";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <RecoilRoot>
-            {/* QueryClientProvider로 애플리케이션을 감싸기  */}
-            <QueryClientProvider client={queryClient}>
-                <GlobalStyle theme={theme} />
-                <RouterProvider router={router} />
-            </QueryClientProvider>
-        </RecoilRoot>
-    </StrictMode>
+  <StrictMode>
+    <RecoilRoot>
+      {/* QueryClientProvider로 애플리케이션을 감싸기  */}
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyle theme={theme} />
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </RecoilRoot>
+  </StrictMode>
 );
