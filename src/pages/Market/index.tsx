d245e55ddbd14f4a5e56158dab6ed21f03ecd52d
OHRRRRR <3819713@naver.com>
2024-10-21 23:42:04 +0900

    feat - 상품전체조회api 연결

--- src/pages/Market/index.tsx
+++ src/pages/Market/index.tsx
@@ -1,3 +1,35 @@
+import React, { useEffect, useState } from "react";
+import axios, { AxiosError } from "axios";
+
 export default function Market() {
-  return <> </>;
+  const [error, setError] = useState<AxiosError | null>(null); // error 타입 명시
+
+  useEffect(() => {
+    // GET 요청 보내기
+    axios
+      .get("http://125.189.109.17/api/products", {
+        params: {
+          name: null,
+          categories: null,
+          cursor: null,
+        },
+      })
+      .then((response) => {
+        // 통신 성공 시 콘솔에 출력
+        console.log("통신 성공:", response.data); // 성공 시 데이터 출력
+      })
+      .catch((error: AxiosError) => {
+        // 에러 처리
+        setError(error);
+        console.error("통신 실패:", error.message); // 실패 시 에러 메시지 출력
+      });
+  }, []);
+
+  if (error) {
+    // 오류가 발생했을 때 에러 메시지 표시
+    return <div>Error: {error.message}</div>;
+  }
+
+  // 화면에 아무 내용도 표시하지 않음
+  return null;
 }
