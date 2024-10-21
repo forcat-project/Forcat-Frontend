1b8dbaba8c172ceedea7004c515469251d017791
ZUITOPIA <korhwngml123@naver.com>
2024-10-21 20:14:01 +0900

    feat: 헤더 구현 진행중

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Block } from "./style/ui";
-// import Footer from "./components/Footer";
 import Navigator from "./components/Navigator";
+import Header from "./components/Header";
 
 function App() {
+    const location = useLocation();
+
+    let pageType: "products" | "search" | "home" | "cart" | "profile" = "home";
+
+    switch (location.pathname) {
+        case "/home":
+            pageType = "home";
+            break;
+        case "/products":
+            pageType = "products";
+            break;
+        case "/cart":
+            pageType = "cart";
+            break;
+        case "/search":
+            pageType = "search";
+            break;
+        case "/profile":
+            pageType = "profile";
+            break;
+        default:
+            pageType = "home";
+            break;
+    }
+
     return (
         <>
             <Block.FlexBox width="599px" height="100%" bgColor="white">
                 <Outlet />
+                <Header pageType={pageType} />
                 <Navigator />
             </Block.FlexBox>
         </>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
-export default function Header() {
-    return <>헤더</>;
+type PageType = "products" | "search" | "home" | "cart" | "profile";
+
+interface HeaderProps {
+    pageType: PageType;
+}
+
+export default function Header({ pageType }: HeaderProps) {
+    return (
+        <>
+            {pageType === "products" && <h1>쇼핑몰 페이지 헤더</h1>}
+            {pageType === "search" && <h1>검색 페이지 헤더</h1>}
+            {pageType === "home" && <h1>홈 페이지 헤더</h1>}
+            {pageType === "cart" && <h1>장바구니 페이지 헤더</h1>}
+            {pageType === "profile" && <h1>프로필 페이지 헤더</h1>}
+        </>
+    );
 }
