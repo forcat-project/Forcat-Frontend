729ff0f96ec8ed439035e4d98889fd9679e67ad5
OHRRRRR <3819713@naver.com>
2024-10-22 13:39:53 +0900

    feat - ChannelIO

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,64 @@
-import { BannerEx } from "../../assets/svg";
-import Footer from "../../components/Footer";
+import React, { useEffect } from "react";
 import { Block, Text } from "../../style/ui";
+import Footer from "../../components/Footer";
+import { BannerEx } from "../../assets/svg";
+
+// window 객체를 확장하여 ChannelIO 속성 추가
+declare global {
+  interface Window {
+    ChannelIO: any;
+    ChannelIOInitialized?: boolean;
+  }
+}
 
 export default function Home() {
+  useEffect(() => {
+    // ChannelIO 스크립트를 동적으로 삽입
+    (function () {
+      if (window.ChannelIO) {
+        return console.error("ChannelIO script included twice.");
+      }
+
+      var ch: any = function () {
+        ch.c(arguments);
+      };
+
+      ch.q = []; // ch에 q 속성 추가
+      ch.c = function (args: any) {
+        ch.q.push(args);
+      }; // ch에 c 속성 추가
+
+      window.ChannelIO = ch;
+
+      function l() {
+        if (window.ChannelIOInitialized) {
+          return;
+        }
+        window.ChannelIOInitialized = true;
+        var s = document.createElement("script");
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://cdn.channel.io/plugin/ch-plugin-web.js";
+        var x = document.getElementsByTagName("script")[0];
+        if (x.parentNode) {
+          x.parentNode.insertBefore(s, x);
+        }
+      }
+
+      if (document.readyState === "complete") {
+        l();
+      } else {
+        window.addEventListener("DOMContentLoaded", l);
+        window.addEventListener("load", l);
+      }
+    })();
+
+    // ChannelIO 플러그인 부트
+    window.ChannelIO("boot", {
+      pluginKey: "4417db19-c908-47ba-bd71-0dcc372639cd",
+    });
+  }, []);
+
   return (
     <>
       <Block.FlexBox
@@ -10,7 +66,7 @@ export default function Home() {
         margin="89px 0 103px 0"
         direction="column"
         justifyContent="space-between"
-        style={{ overflow: "scroll", scrollbarWidth: "none" }} // Footer와 margin을 고려한 높이 설정
+        style={{ overflow: "scroll", scrollbarWidth: "none" }}
       >
         <Block.FlexBox width="599px" height="277px" bgColor="black">
           <BannerEx />
@@ -23,35 +79,7 @@ export default function Home() {
           style={{ height: "1000px" }}
         >
           <Block.FlexBox width="100%" height="800px">
-            <Text.Menu>
-              imply dummy text of the printing and typesetting industry. Lorem
-              Ipsum has been the industry's standard dummy text ever since the
-              1500s, when an unknown printer took a galley of type and scrambled
-              it to make a type specimen book. It has survived not only five
-              centuries, but also the leap into electronic typesetting,
-              remaining essentially unchanged. It was popularised in the 1960s
-            </Text.Menu>
-          </Block.FlexBox>
-          <Block.FlexBox width="100%" height="800px">
-            <Text.Menu>
-              imply dummy text of the printing and typesetting industry. Lorem
-              Ipsum has been the industry's standard dummy text ever since the
-              1500s, when an unknown printer took a galley of type and scrambled
-              it to make a type specimen book. It has survived not only five
-              centuries, but also the leap into electronic typesetting,
-              remaining essentially unchanged. It was popularised in the 1960s
-            </Text.Menu>
-          </Block.FlexBox>
-
-          <Block.FlexBox width="100%" height="800px">
-            <Text.Menu>
-              imply dummy text of the printing and typesetting industry. Lorem
-              Ipsum has been the industry's standard dummy text ever since the
-              1500s, when an unknown printer took a galley of type and scrambled
-              it to make a type specimen book. It has survived not only five
-              centuries, but also the leap into electronic typesetting,
-              remaining essentially unchanged. It was popularised in the 1960s
-            </Text.Menu>
+            <Text.Menu>Some dummy text for the example.</Text.Menu>
           </Block.FlexBox>
         </Block.FlexBox>
 
