abd47c70e4b377d3261f7ed90266c9908ae0a7d7
ZUITOPIA <korhwngml123@naver.com>
2024-10-24 13:52:08 +0900

    fix: gitignore 수정

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,15 +2,16 @@ import { BtnGoogle, BtnKakao, BtnNaver, LoginLogo } from "../../assets/svg";
 import { Block, Text } from "../../style/ui";
 
 export default function Login() {
+    const handleKakaoLoginClick = () => {};
     return (
         <>
             <Block.FlexBox direction="column" alignItems="center" justifyContent="center">
                 <Text.TitleMenu100>반려인 필수!</Text.TitleMenu100>
                 <Text.TitleMenu300>고양이 커머스 1등 앱</Text.TitleMenu300>
                 <LoginLogo width={108} height={59} />
-                <Text.TitleMenu100>SNS 계정으로 로그인</Text.TitleMenu100>
+                <Text.TitleMenu100 color="Gray">간편 로그인</Text.TitleMenu100>
                 <Block.FlexBox justifyContent="center" gap="20px">
-                    <BtnKakao width={46} />
+                    <BtnKakao onClick={handleKakaoLoginClick} width={46} />
                     <BtnGoogle width={46} />
                     <BtnNaver width={46} />
                 </Block.FlexBox>
