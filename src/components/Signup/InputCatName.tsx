29ae1d5c311415aa947cf24ac6b831f93c6c5e22
OHRRRRR <3819713@naver.com>
2024-10-29 17:28:30 +0900

    fix: 배포 오류 수정7

--- src/components/Signup/InputCatName.tsx
+++ src/components/Signup/InputCatName.tsx
@@ -4,25 +4,25 @@ import { Block, Input, Text } from "../../style/ui";
 import { catState } from "../../recoil";
 
 export default function InputCatName() {
-    const [catInfo, setCatInfo] = useRecoilState(catState);
-    const { isFocused, handleFocus, handleBlur } = useFocus();
+  const [catInfo, setCatInfo] = useRecoilState(catState);
+  const { isFocused, handleFocus, handleBlur } = useFocus();
 
-    const handleUserCatNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setCatInfo(prev => ({ ...prev, name: e.target.value }));
-    };
+  const handleUserCatNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCatInfo((prev) => ({ ...prev, name: e.target.value }));
+  };
 
-    return (
-        <>
-            <Block.FlexBox direction="column" gap="20px">
-                <Text.FocusedMenu isFocused={isFocused}>이름</Text.FocusedMenu>
-                <Input.InfoBox
-                    value={catInfo.name}
-                    onFocus={handleFocus}
-                    onBlur={handleBlur}
-                    onChange={handleUserCatNameChange}
-                    placeholder="고양이 이름을 입력해주세요"
-                />
-            </Block.FlexBox>
-        </>
-    );
+  return (
+    <>
+      <Block.FlexBox direction="column" gap="20px">
+        <Text.FocusedMenu isFocused={isFocused}>이름</Text.FocusedMenu>
+        <Input.InfoBox
+          value={catInfo.name}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          onChange={handleUserCatNameChange}
+          placeholder="고양이 이름을 입력해주세요"
+        />
+      </Block.FlexBox>
+    </>
+  );
 }
--- src/components/Signup/InputUserNickName.tsx
+++ src/components/Signup/InputUserNickName.tsx
@@ -4,23 +4,23 @@ import { Block, Input, Text } from "../../style/ui";
 import { userState } from "../../recoil";
 
 export default function InputUserNickName() {
-    const [userInfo, setUserInfo] = useRecoilState(userState);
-    const { isFocused, handleFocus, handleBlur } = useFocus();
+  const [, setUserInfo] = useRecoilState(userState);
+  const { isFocused, handleFocus, handleBlur } = useFocus();
 
-    const handleUserNickNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setUserInfo(prev => ({ ...prev, nickname: e.target.value }));
-    };
-    return (
-        <>
-            <Block.FlexBox direction="column" gap="20px">
-                <Text.FocusedMenu isFocused={isFocused}>닉네임</Text.FocusedMenu>
-                <Input.InfoBox
-                    onFocus={handleFocus}
-                    onBlur={handleBlur}
-                    onChange={handleUserNickNameChange}
-                    placeholder="닉네임을 입력해주세요"
-                />
-            </Block.FlexBox>
-        </>
-    );
+  const handleUserNickNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserInfo((prev) => ({ ...prev, nickname: e.target.value }));
+  };
+  return (
+    <>
+      <Block.FlexBox direction="column" gap="20px">
+        <Text.FocusedMenu isFocused={isFocused}>닉네임</Text.FocusedMenu>
+        <Input.InfoBox
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          onChange={handleUserNickNameChange}
+          placeholder="닉네임을 입력해주세요"
+        />
+      </Block.FlexBox>
+    </>
+  );
 }
