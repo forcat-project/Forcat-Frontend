f65b1878ed4b08f4a3da0bde89587c73d4d34015
ZUITOPIA <korhwngml123@naver.com>
2024-10-29 16:10:47 +0900

    feat: 중성화 수술 여부 입력

---
+++ src/components/Signup/InputCatIsNeutered.tsx
@@ -0,0 +1,47 @@
+import { useRecoilState } from "recoil";
+import { Block, Button, Text } from "../../style/ui";
+import { catState } from "../../recoil";
+import { useState } from "react";
+
+export default function InputCatIsNeutered() {
+    const [catInfo, setCatInfo] = useRecoilState(catState);
+    const [isNeutered, setIsNeutered] = useState(true);
+
+    const [isFocused, setIsFocused] = useState(false);
+
+    const handleIsNeuteredClick = (isNeutered: boolean) => {
+        setIsNeutered(isNeutered);
+        setCatInfo(prev => ({
+            ...prev,
+            is_neutered: isNeutered ? 1 : 0,
+        }));
+        setIsFocused(true);
+    };
+
+    return (
+        <Block.FlexBox direction="column" gap="20px">
+            <Text.FocusedMenu isFocused={isFocused}>중성화</Text.FocusedMenu>
+
+            <Block.FlexBox justifyContent="space-between">
+                <Button.SelectInput
+                    tabIndex={0}
+                    onClick={() => handleIsNeuteredClick(true)}
+                    onFocus={() => setIsFocused(true)}
+                    onBlur={() => setIsFocused(false)}
+                    isActive={isNeutered}
+                >
+                    했어요
+                </Button.SelectInput>
+                <Button.SelectInput
+                    tabIndex={0}
+                    onClick={() => handleIsNeuteredClick(false)}
+                    onFocus={() => setIsFocused(true)}
+                    onBlur={() => setIsFocused(false)}
+                    isActive={!isNeutered}
+                >
+                    안 했어요
+                </Button.SelectInput>
+            </Block.FlexBox>
+        </Block.FlexBox>
+    );
+}
