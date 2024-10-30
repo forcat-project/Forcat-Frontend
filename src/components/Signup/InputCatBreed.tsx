299bcde808e4d4db076668a46ced50d893f35b04
ZUITOPIA <korhwngml123@naver.com>
2024-10-30 15:21:19 +0900

    feat: 포캣 모달 컴포넌트 생성

--- src/components/Signup/InputCatBreed.tsx
+++ src/components/Signup/InputCatBreed.tsx
@@ -1,22 +1,37 @@
-// import { useRecoilState } from "recoil";
+import { useState } from "react";
 import useFocus from "../../hooks/useFocus";
 import { Block, Input, Text } from "../../style/ui";
-// import { catState } from "../../recoil";
+import ForcatModal from "../Modal/ForcatModal";
 
 export default function InputCatBreed() {
-  // const [catInfo, setCatInfo] = useRecoilState(catState);
-  const { isFocused, handleFocus, handleBlur } = useFocus();
+    const { isFocused, handleFocus, handleBlur } = useFocus();
 
-  return (
-    <>
-      <Block.FlexBox direction="column" gap="20px">
-        <Text.FocusedMenu isFocused={isFocused}>묘종</Text.FocusedMenu>
-        <Input.InfoBox
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-          placeholder="선택해주세요"
-        />
-      </Block.FlexBox>
-    </>
-  );
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleBreedSelectButtonClick = () => {
+        setIsModalOpen(true);
+    };
+
+    return (
+        <>
+            <ForcatModal
+                isOpen={isModalOpen}
+                setIsOpen={setIsModalOpen}
+                width="599px"
+                height="700px"
+                title="모달타이틀"
+            >
+                내용
+            </ForcatModal>
+            <Block.FlexBox direction="column" gap="20px">
+                <Text.FocusedMenu isFocused={isFocused}>묘종</Text.FocusedMenu>
+                <Input.InfoBox
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                    placeholder="선택해주세요"
+                    onClick={handleBreedSelectButtonClick}
+                />
+            </Block.FlexBox>
+        </>
+    );
 }
