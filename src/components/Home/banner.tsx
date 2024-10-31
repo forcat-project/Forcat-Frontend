af3e5f52fcd28a18487f20530cee00da3621a97a
OHRRRRR <3819713@naver.com>
2024-10-31 09:08:31 +0900

    design: banner 이미지 완료

--- src/components/Home/banner.tsx
+++ src/components/Home/banner.tsx
@@ -1,5 +1,5 @@
-import Slider from "react-slick"; // react-slick 라이브러리 import
-import { Banner1, Banner2, Banner3 } from "../../assets/svg";
+import Slider from "react-slick"; // react-slick
+// import { Banner1, Banner2, Banner3 } from "../../assets/svg";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -17,14 +17,19 @@ const settings = {
 };
 
 // 배너 이미지를 배열로 정의
-const banners = [<Banner1 />, <Banner2 />, <Banner3 />];
+// const banners = [<Banner1 />, <Banner2 />, <Banner3 />];
+const banners = [
+  "/banners/banner1.jpg",
+  "/banners/banner2.jpg",
+  "/banners/banner3.jpg",
+];
 
 // 슬라이더 컴포넌트 정의
 export default function BannerSlider() {
   return (
     <div style={{ width: "100%", maxWidth: "1200px", margin: "0 auto" }}>
       <Slider {...settings}>
-        {banners.map((banner, index) => (
+        {banners.map((src, index) => (
           <div
             key={index}
             style={{
@@ -34,7 +39,16 @@ export default function BannerSlider() {
               height: "400px", // 높이 조정
             }}
           >
-            {banner}
+            <img
+              src={src}
+              alt={`Banner ${index + 1}`}
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                // borderRadius: "8px",
+              }}
+            />
           </div>
         ))}
       </Slider>
