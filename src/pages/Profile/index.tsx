a361a2ea277a3e64e35f2bd0086deacd4f495177
ZUITOPIA <korhwngml123@naver.com>
2024-10-31 10:52:44 +0900

    feat: 프로필 페이지에 있다가 accessToken 잃어버리면 login 페이지로 가도록 수정

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,32 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Block } from "../../style/ui";
 import UserInfo from "../../components/Profile/userInfo";
 import Cats from "../../components/Profile/cats";
 import Purchase from "../../components/Profile/purchase";
 import AccountManage from "../../components/Profile/accountManage";
 import { PageWrapper } from "../../style/ui";
+import { getCookie } from "../../api/cookie";
+import { useNavigate } from "react-router-dom";
 
 export default function Profile() {
-  const [reload, setReload] = useState(false);
+    const [reload, setReload] = useState(false);
 
-  const handleReload = () => {
-    setReload(!reload);
-  };
+    const handleReload = () => {
+        setReload(!reload);
+    };
 
-  return (
-    <PageWrapper>
-      <Block.FlexBox
-        direction="column"
-        padding="20px 0"
-        maxWidth="600px"
-        margin="80px auto 100px"
-        key={reload.toString()} // `key`에 문자열로 변경
-      >
-        <UserInfo onReload={handleReload} />
-        <Cats />
-        <Purchase />
-        <AccountManage />
-      </Block.FlexBox>
-    </PageWrapper>
-  );
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (!getCookie("access_token")) {
+            navigate("/login");
+        }
+    }, []);
+
+    return (
+        <PageWrapper>
+            <Block.FlexBox
+                direction="column"
+                padding="20px 0"
+                maxWidth="600px"
+                margin="80px auto 100px"
+                key={reload.toString()} // `key`에 문자열로 변경
+            >
+                <UserInfo onReload={handleReload} />
+                <Cats />
+                <Purchase />
+                <AccountManage />
+            </Block.FlexBox>
+        </PageWrapper>
+    );
 }
