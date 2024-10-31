7f020f513deff4b51252407b622569f15f9f8674
OHRRRRR <3819713@naver.com>
2024-10-31 16:01:37 +0900

    feat: 스크롤 위치 저장 구현중

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,45 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { Block } from "./style/ui";
 import Navigator from "./components/Navigator";
 import Header from "./components/Header/Header";
-import { useNavigate } from "react-router-dom";
+import useScrollRestoration from "./hooks/useScrollRestoration"; // 커스텀 훅 import
 
 function App() {
-    const navigate = useNavigate();
-    const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    const isMarketDetailPage = /^\/market\/\d+$/.test(location.pathname);
-    const isLoginPage = location.pathname.startsWith("/login");
-    const isSignupPage = location.pathname.startsWith("/signup");
+  const isMarketDetailPage = /^\/market\/\d+$/.test(location.pathname);
+  const isLoginPage = location.pathname.startsWith("/login");
+  const isSignupPage = location.pathname.startsWith("/signup");
 
-    const handleBackButtonClick = () => {
-        navigate(-1);
-    };
+  useScrollRestoration(); // 페이지 전환 시 스크롤 복원
 
-    const handleProfileClick = () => {
-        navigate("/login");
-    };
+  const handleBackButtonClick = () => {
+    navigate(-1);
+  };
 
-    return (
-        <>
-            <Block.FlexBox width="599px" height="100%" bgColor="white">
-                <Header
-                    isLoginPage={isLoginPage}
-                    isSignupPage={isSignupPage}
-                    handleBackButtonClick={handleBackButtonClick}
-                    handleProfileClick={handleProfileClick}
-                />
-                <Outlet />
-                <Navigator
-                    isLoginPage={isLoginPage}
-                    isSignupPage={isSignupPage}
-                    isMarketDetailPage={isMarketDetailPage}
-                />
-            </Block.FlexBox>
-        </>
-    );
+  const handleProfileClick = () => {
+    navigate("/login");
+  };
+
+  return (
+    <>
+      <Block.FlexBox width="599px" height="100%" bgColor="white">
+        <Header
+          isLoginPage={isLoginPage}
+          isSignupPage={isSignupPage}
+          handleBackButtonClick={handleBackButtonClick}
+          handleProfileClick={handleProfileClick}
+        />
+        <Outlet />
+        <Navigator
+          isLoginPage={isLoginPage}
+          isSignupPage={isSignupPage}
+          isMarketDetailPage={isMarketDetailPage}
+        />
+      </Block.FlexBox>
+    </>
+  );
 }
 
 export default App;
