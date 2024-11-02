aca366f124edcd1020bbbf24fadabe996cf1f0e9
OHRRRRR <3819713@naver.com>
2024-11-03 02:40:10 +0900

    fix: 이미지 수정

--- src/pages/Profile/privacyPolicy.tsx
+++ src/pages/Profile/privacyPolicy.tsx
@@ -1,26 +1,16 @@
 import { PageWrapper } from "../../style/ui";
 import styled from "styled-components";
 
-// 이미지 파일을 import합니다.
-import privacyPolicy1 from "../../../public/PrivacyPolicy/개인정보처리방침1.png";
-import privacyPolicy2 from "../../../public/PrivacyPolicy/개인정보처리방침2.png";
-import privacyPolicy3 from "../../../public/PrivacyPolicy/개인정보처리방침3.png";
-import privacyPolicy4 from "../../../public/PrivacyPolicy/개인정보처리방침4.png";
-import privacyPolicy5 from "../../../public/PrivacyPolicy/개인정보처리방침5.png";
-import privacyPolicy6 from "../../../public/PrivacyPolicy/개인정보처리방침6.png";
-import privacyPolicy7 from "../../../public/PrivacyPolicy/개인정보처리방침7.png";
-import privacyPolicy8 from "../../../public/PrivacyPolicy/개인정보처리방침8.png";
-
 // 이미지 배열을 설정합니다.
 const images = [
-  privacyPolicy1,
-  privacyPolicy2,
-  privacyPolicy3,
-  privacyPolicy4,
-  privacyPolicy5,
-  privacyPolicy6,
-  privacyPolicy7,
-  privacyPolicy8,
+  "../../../public/PrivacyPolicy/개인정보처리방침1.png",
+  "../../../public/PrivacyPolicy/개인정보처리방침2.png",
+  "../../../public/PrivacyPolicy/개인정보처리방침3.png",
+  "../../../public/PrivacyPolicy/개인정보처리방침4.png",
+  "../../../public/PrivacyPolicy/개인정보처리방침5.png",
+  "../../../public/PrivacyPolicy/개인정보처리방침6.png",
+  "../../../public/PrivacyPolicy/개인정보처리방침7.png",
+  "../../../public/PrivacyPolicy/개인정보처리방침8.png",
 ];
 
 export default function TermsOfUse() {
--- src/pages/Profile/termsofUse.tsx
+++ src/pages/Profile/termsofUse.tsx
@@ -1,24 +1,14 @@
 import { PageWrapper } from "../../style/ui";
 import styled from "styled-components";
 
-// 이미지 파일을 import합니다.
-import termsofUse1 from "../../../public/TermsofUse/이용약관1.png";
-import termsofUse2 from "../../../public/TermsofUse/이용약관2.png";
-import termsofUse3 from "../../../public/TermsofUse/이용약관3.png";
-import termsofUse4 from "../../../public/TermsofUse/이용약관4.png";
-import termsofUse5 from "../../../public/TermsofUse/이용약관5.png";
-import termsofUse6 from "../../../public/TermsofUse/이용약관6.png";
-import termsofUse7 from "../../../public/TermsofUse/이용약관7.png";
-
-// 이미지 배열을 설정합니다.
 const images = [
-  termsofUse1,
-  termsofUse2,
-  termsofUse3,
-  termsofUse4,
-  termsofUse5,
-  termsofUse6,
-  termsofUse7,
+  "../../../public/TermsofUse/이용약관1.png",
+  "../../../public/TermsofUse/이용약관2.png",
+  "../../../public/TermsofUse/이용약관3.png",
+  "../../../public/TermsofUse/이용약관4.png",
+  "../../../public/TermsofUse/이용약관5.png",
+  "../../../public/TermsofUse/이용약관6.png",
+  "../../../public/TermsofUse/이용약관7.png",
 ];
 
 export default function TermsOfUse() {
